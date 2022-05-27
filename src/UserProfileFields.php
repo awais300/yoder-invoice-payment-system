ad4813666a420d1d21d1caad8c5fe023fa085519
Awais <awais300@gmail.com>
2022-05-28 03:52:06 +0500

    setup new user custom email

--- src/UserProfileFields.php
+++ src/UserProfileFields.php
@@ -13,6 +13,7 @@ class UserProfileFields
 {
     private $loader = null;
     public const META_CUSTOMER_SAGE_FIELD = 'customer_sage_id';
+    public const FROM_EMAIL = 'no-reply@yoderoil.com';
 
     public function __construct()
     {
@@ -24,6 +25,8 @@ class UserProfileFields
         add_action('user_register', array($this, 'yoder_save_profile_fields'));
         add_action('personal_options_update', array($this, 'yoder_save_profile_fields'));
         add_action('edit_user_profile_update', array($this, 'yoder_save_profile_fields'));
+
+        add_filter('wp_new_user_notification_email', array($this, 'yoder_wp_new_user_notification_email'), 10, 3);
     }
 
     public function yoder_register_profile_fields($user)
@@ -52,5 +55,17 @@ class UserProfileFields
         }
 
         update_user_meta($user_id, self::META_CUSTOMER_SAGE_FIELD, $_POST[self::META_CUSTOMER_SAGE_FIELD]);
+    }
+
+    public function yoder_wp_new_user_notification_email($new_user_email, $user, $blogname)
+    {
+        $message  = sprintf( __( 'Username: %s' ), $user->user_email ) . "<br/><br/>";
+        $message .= __( 'To set your password, visit the following address:' ) . "<br/><br/>";
+        $message .= network_site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user->user_login ), 'login' ) . "<br/><br/>";
+
+        $new_user_email['subject'] = sprintf('Welcome to %s', $blogname);
+        $new_user_email['message'] = $message;
+        $new_user_email['headers'] = Helper::get_headers_for_email(self::FROM_EMAIL);
+        return $new_user_email;
     }
 }
