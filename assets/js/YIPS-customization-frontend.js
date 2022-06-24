a51eb7e9a2edcf5b7efba3939a99beb2c6979e1a
Awais <awais300@gmail.com>
2022-06-25 00:49:43 +0500

    manage background based on checkbox value

--- assets/js/YIPS-customization-frontend.js
+++ assets/js/YIPS-customization-frontend.js
@@ -2,6 +2,13 @@ jQuery(document).ready(function($) {
     $(document).on('click', '#yoder-pay-online .invoice-box', function(e) {
         total = calculate_total_fee();
         display_totals(total);
+
+        $this = $(this);
+        if($this.is(':checked') === true) {
+            box_checked($this);
+        } else {
+            box_unchecked($this);
+        }
     });
 
     function calculate_total_fee() {
@@ -22,7 +29,6 @@ jQuery(document).ready(function($) {
     }
 
     function display_totals(total) {
-
         // Total fee
         total = total.toFixed(2);
 
@@ -31,11 +37,20 @@ jQuery(document).ready(function($) {
         cfee = cfee.toFixed(2);
 
         total_fee = Number(total) + Number(cfee);
+        total_fee = total_fee.toFixed(2);
 
         $('#yoder-pay-online .fee span').text(total_fee);
         $('#yoder-pay-online #total_fee').val(total_fee);
 
         $('#yoder-pay-online .cfee span').text(cfee);
         $('#yoder-pay-online #convenience_fee').val(cfee);
+    }
+
+    function box_checked($ele) {
+        $ele.parent().parent().css('background-color', '#ffffff');
+    }
+
+    function box_unchecked($ele) {
+        $ele.parent().parent().css('background-color', 'inherit');
     }
 });
