81908a2effb04748cafb3439aedc0c048c294271
Awais <awais300@gmail.com>
2022-06-25 00:57:08 +0500

    abstract singleton class

---
+++ src/Singleton.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Yoder\YIPS;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Base class for singleton objects.
+ * Class Singleton.
+ * @package Yoder\YIPS
+ * @abstract
+ */
+abstract class Singleton {
+
+	private static $instances = array();
+	protected function __construct() {}
+	protected function __clone() {}
+	public function __wakeup() {
+		throw new \Exception( 'Cannot unserialize singleton' );
+	}
+
+
+	/**
+	 * Get instance object.
+	 **/
+	public static function get_instance() {
+		$cls = get_called_class(); // late-static-bound class name
+		if ( ! isset( self::$instances[ $cls ] ) ) {
+			self::$instances[ $cls ] = new static;
+			( self::$instances[ $cls ] )->on_construct();
+		}
+		return self::$instances[ $cls ];
+	}
+
+	function on_construct() {}
+
+	/**
+	 * Returns the singleton instance.
+	 *
+	 * @return $this
+	 */
+	public static function instance() {
+		 return static::get_instance();
+	}
+}
