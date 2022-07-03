a048d8867d7a9ecb815cb7dc626b1886294c5b32
Awais <awais300@gmail.com>
2022-07-03 09:41:31 +0500

    add user_has_access method

--- src/User/UserRoles.php
+++ src/User/UserRoles.php
@@ -2,6 +2,8 @@
 
 namespace Yoder\YIPS\User;
 
+use Yoder\YIPS\Singleton;
+
 defined('ABSPATH') || exit;
 
 /**
@@ -9,16 +11,62 @@ defined('ABSPATH') || exit;
  * @package Yoder\YIPS
  */
 
-class UserRoles
+class UserRoles extends Singleton
 {
+    /**
+     * User custom role.
+     *
+     * @var ROLE_YODER_INVOICE_CUSTOMER
+     */
     public const ROLE_YODER_INVOICE_CUSTOMER = 'yoder-invoice-customer';
-    
-    public static function add_role() {
+
+    /**
+     * Add user to custom role.
+     *
+     */
+    public function add_role()
+    {
         remove_role(self::ROLE_YODER_INVOICE_CUSTOMER);
-        add_role( self::ROLE_YODER_INVOICE_CUSTOMER, 'Yoder Invoice Customer', get_role( 'subscriber' )->capabilities );
+        add_role(
+            self::ROLE_YODER_INVOICE_CUSTOMER,
+            'Yoder Invoice Customer',
+            get_role('subscriber')->capabilities
+        );
     }
 
-    public static function remove_role() {
+    /**
+     * Remove custom role.
+     *
+     */
+    public function remove_role()
+    {
         remove_role(self::ROLE_YODER_INVOICE_CUSTOMER);
+    }
+
+    /**
+     * Check whether user has required access.
+     * 
+     * @return boolean
+     */
+    public function user_has_access()
+    {
+        if (!is_user_logged_in()) {
+            return false;
+        }
+
+        $allowed_roles = array('administrator', self::ROLE_YODER_INVOICE_CUSTOMER);
+        $user_roles = wp_get_current_user()->roles;
+
+        if (empty($user_roles)) {
+            return false;
+        }
+
+        foreach ($user_roles as $role) {
+            if (in_array($role, $allowed_roles)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
