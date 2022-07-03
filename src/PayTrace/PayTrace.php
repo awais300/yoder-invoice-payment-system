938dd34a7d97d0a3df36651ff010dfb058b6fccd
Awais <awais300@gmail.com>
2022-07-03 09:37:57 +0500

    add docblock

--- src/PayTrace/PayTrace.php
+++ src/PayTrace/PayTrace.php
@@ -5,6 +5,9 @@ namespace Yoder\YIPS\PayTrace;
 use Yoder\YIPS\Singleton;
 use Yoder\YIPS\WPLogger;
 use Yoder\YIPS\Helper;
+use Yoder\YIPS\Invoice\Invoice;
+use Yoder\YIPS\User\UserMeta;
+use Yoder\YIPS\Rosetta\Rosetta;
 
 defined('ABSPATH') || exit;
 
@@ -15,15 +18,33 @@ defined('ABSPATH') || exit;
 
 class PayTrace extends Singleton
 {
+    /**
+     * Log information to file.
+     *
+     * @var $logger
+     */
     private $logger = null;
+
+    /**
+     * Contains utility functions for PayTrace.
+     *
+     * @var $logger
+     */
     private $util = null;
 
+    /**
+     * Construct the plugin.
+     */
     public function __construct()
     {
         $this->logger = WPLogger::instance();
         $this->util = Utilities::instance();
     }
 
+    /**
+     * Get client key via PayTrace API.
+     * @return array
+     */
     public function get_client_key()
     {
         $result = array(
@@ -39,12 +60,12 @@ class PayTrace extends Singleton
             $this->logger->log('get_client_key(): $is_error');
             $this->logger->log($this->util->getFoundOAuthTokenError($oauth_result));
             $this->logger->log($oauth_result['response']);
-            $result['error_message'] = "Couldn't create a PayTrace connection to process payment. Please contact administrator";
+            $result['error_message'] = __("Couldn't create a PayTrace connection to process payment. Please contact administrator", 'yips-customization');
             $result['client_key'] = '';
             return $result;
         }
 
-        $json = Helper::jsonDecode($oauth_result['temp_json_response']);
+        $json = (Helper::instance())->jsonDecode($oauth_result['temp_json_response']);
         $oauth_token = sprintf("Bearer %s", $json['access_token']);
 
         // Protect oauth to genreate key
@@ -69,12 +90,18 @@ class PayTrace extends Singleton
             }
         }
 
-        $json = Helper::jsonDecode($api_result['temp_json_response']);
+        $json = (Helper::instance())->jsonDecode($api_result['temp_json_response']);
         $result['client_key'] = $json['clientKey'];
         $result['error_message'] = '';
         return $result;
     }
 
+    /**
+     * Process payment when invoice form is submit.
+     * on success redirect to thank you page on failure return error.
+     *
+     * @return Array contains error info.
+     */
     public function process_payment()
     {
 
@@ -103,16 +130,34 @@ class PayTrace extends Singleton
             $this->logger->log('process_payment(): $is_error');
             $this->logger->log($this->util->getFoundOAuthTokenError($oauth_result));
             $this->logger->log($oauth_result['response']);
-            $result['error_message'] = "Couldn't create a PayTrace connection to process payment. Please contact administrator";
+            $result['error_message'] = __("Couldn't create a PayTrace connection to process payment. Please contact administrator", 'yips-customization');
             $result['transaction_success'] = false;
             return $result;
         }
 
-        $json = Helper::jsonDecode($oauth_result['temp_json_response']);
+        $json = (Helper::instance())->jsonDecode($oauth_result['temp_json_response']);
         $oauth_token = sprintf("Bearer %s", $json['access_token']);
-        return $this->buildTransaction($oauth_token);
+        $transaction_result = $this->buildTransaction($oauth_token);
+
+
+        if (isset($transaction_result['transaction_success']) && $transaction_result['transaction_success'] === true) {
+            // Transaction success.
+            $invoices['invoice'] = array_keys($_POST['invoice']);
+            $invoices['user_id'] = get_current_user_id();
+            (UserMeta::instance())->save_user_invoice_data(get_current_user_id(), $invoices);
+            wp_redirect(('/' . Invoice::THANK_YOU_PAGE));
+            exit;
+        } else {
+            return $transaction_result;
+        }
     }
 
+    /**
+     * Build transaction and process it via API.
+     *
+     * @param string $oauth_token.
+     * @return Array
+     */
     public function buildTransaction($oauth_token)
     {
         $request_data = $this->buildRequestData();
@@ -120,9 +165,15 @@ class PayTrace extends Singleton
         return $this->verifyTransactionResult($result);
     }
 
-
+    /**
+     * Build request data to be sent via PayTrace API.
+     * @return Array
+     */
     public function buildRequestData()
     {
+        $customer = (Rosetta::instance())->get_customer();
+        $customer = $customer['customer'];
+
         $hpf_token = $_POST['HPF_Token'];
         $enc_key = $_POST['enc_key'];
         $amount = $_POST['amount'];
@@ -132,11 +183,12 @@ class PayTrace extends Singleton
             "enc_key" => $enc_key,
             "integrator_id" => "967174xd2CvC",
             "billing_address" => array(
-                "name" => "Test-Protect-Sale",
-                "street_address" => "8320 E. West St.",
-                "city" => "Spokane",
-                "state" => "WA",
-                "zip" => "85284"
+                "name" => $customer['CustomerName'],
+                "street_address" => $customer['AddressLine1'],
+                "street_address2" => $customer['AddressLine2'],
+                "city" => $customer['City'],
+                "state" => $customer['State'],
+                "zip" => $customer['ZipCode']
             )
         );
 
@@ -152,7 +204,12 @@ class PayTrace extends Singleton
         return $request_data;
     }
 
-    //This function is to verify the Transaction result
+    /**
+     * Verify the transaction and return the response.
+     *
+     * @param Array $trans_result.
+     * @return Array
+     */
     public function verifyTransactionResult($trans_result)
     {
         $result = array(
@@ -170,19 +227,19 @@ class PayTrace extends Singleton
         //If we reach here, we have been able to communicate with the service,
         //next is decode the json response and then review Http Status code, response_code and success of the response
 
-        $json = Helper::jsonDecode($trans_result['temp_json_response']);
+        $json = (Helper::instance())->jsonDecode($trans_result['temp_json_response']);
 
         if ($trans_result['http_status_code'] != 200) {
             if ($json['success'] === false) {
                 $this->logger->log('verifyTransactionResult(): $json===false');
                 $this->logger->log($trans_result['response']);
-                $result['error_message'] = 'Transaction Error occurred. Please try again or contact administrator.';
+                $result['error_message'] = __('Transaction Error occurred. Please try again or contact administrator.', 'yips-customization');
                 $result['transaction_success'] = false;
                 return $result;
             } else {
                 $this->logger->log('verifyTransactionResult(): $json===else');
                 $this->logger->log($trans_result['response']);
-                $result['error_message'] = 'Request Error occurred. Please try again or contact administrator.';
+                $result['error_message'] = __('Request Error occurred. Please try again or contact administrator.', 'yips-customization');
                 $result['transaction_success'] = false;
                 return $result;
             }
@@ -193,6 +250,7 @@ class PayTrace extends Singleton
             if ($json['success'] == true && $json['response_code'] == 101) {
                 $result['error_message'] = '';
                 $result['transaction_success'] = true;
+                $result['transation_response'] = $trans_result;
                 return $result;
             } else {
                 $this->logger->log('verifyTransactionResult(): else');
