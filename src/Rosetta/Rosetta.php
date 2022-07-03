3a3c5c845753f09f8af6ea6af80932aad81f15eb
Awais <awais300@gmail.com>
2022-07-03 09:39:36 +0500

    add docblock and use socket

--- src/Rosetta/Rosetta.php
+++ src/Rosetta/Rosetta.php
@@ -4,6 +4,9 @@ namespace Yoder\YIPS\Rosetta;
 
 use Yoder\YIPS\Singleton;
 use Yoder\YIPS\Helper;
+use Yoder\YIPS\User\UserMeta;
+use Yoder\YIPS\WPLogger;
+use Yoder\YIPS\Config;
 
 defined('ABSPATH') || exit;
 
@@ -14,141 +17,100 @@ defined('ABSPATH') || exit;
 
 class Rosetta extends Singleton
 {
-    public function __construct()
+    /**
+     * Log information.
+     *
+     * @var $logger
+     */
+    private $logger = null;
+
+    /**
+     * Contains plugin configuration.
+     *
+     * @var $logger
+     */
+    private $config = null;
+
+    /**
+     * Construct the plugin.
+     */
+    public function on_construct()
     {
-        $aa = $this->setting();
-        //$aa = $this->get_customer();
-        
-        if(isset($_GET['yoder_api_debug'])) {
-            dd($aa);
+        $this->config = (Config::instance())->get_config('rosetta');
+        $this->logger = WPLogger::instance();
+
+        if (isset($_GET['yoder_api_debug'])) {
+            $test = $this->test();
+            dd($test);
             exit();
         }
-       
     }
 
-    public function setting()
+    public function test()
     {
-        $result = array();
-       /* $xml = '<dm2RosettaSettings>
-                  <setting>
-                    <securitykey>32B32BCE9E61CD42AB89622D495B2</securitykey>
-                    <user>ros</user>
-                    <password>ros1234</password>
-                    <maspath>C:\SAGE100A\MAS90\HOME</maspath>
-                    <companycode>TST</companycode>
-                  </setting>
-                </dm2RosettaSettings>';*/
 
-        $xml = '<rosetta>
-                    <securitykey>32B32BCE9E61CD42AB89622D495B2</securitykey>
-                    <function>GetOpenInvoiceData</function>
-  <xml>
-    <ExtSourceID>YODERWEB</ExtSourceID>
-    <ARDivisionNo>00</ARDivisionNo>
-    <CustomerNo>0000004</CustomerNo>
-  </xml>
-</rosetta>';
+        $xml = "<rosetta>
+          <securitykey>{$this->config['security_key']}</securitykey>
+          <function>GetCustomerData</function>
+          <xml>
+            <ExtSourceID>YODERWEB</ExtSourceID>
+            <ARDivisionNo>00</ARDivisionNo>
+            <CustomerNo>0001067</CustomerNo>
+          </xml>
+        </rosetta>";
 
-
-$xml = '<rosetta>
-<securitykey>32B32BCE9E61CD42AB89622D495B2</securitykey>
-  <function>GetCustomerData</function>
-  <xml>
-    <ExtSourceID>YODERWEB</ExtSourceID>
-    <ARDivisionNo>00</ARDivisionNo>
-    <CustomerNo>0000004</CustomerNo>
-  </xml>
-</rosetta>';
-        
-        $headers = array(
-            'Authorization' => 'Basic ' . base64_encode( '' . ':' . '' )
-        );
-
-        $headers = array();
-
-        $url = 'http://50.240.141.89';
-        $request_data = $xml;
-        $response = wp_remote_get(
-            $url,
-            array(
-                'method'      => 'GET',
-                'timeout'     => 45,
-                'redirection' => 5,
-                'blocking'    => true,
-                'sslverify'    => false,
-                'body'        => $request_data
-            )
-        );
-
-        if (is_wp_error($response)) {
-            $result['success'] = false;
-            $result['curl_error'] = $response->get_error_message();
-        } else {
-            $result['success'] = true;
-        }
-
-        $result['response'] = $response;
-        return $result;
+        $response = $this->yoder_remote_get($xml);
+        return $response;
     }
 
+    /**
+     * Get customer invoices.
+     */
     public function get_invoices()
     {
-
-        $xml = '<rosetta>
-  <ErrorStatus>0</ErrorStatus>
-  <ErrorMessage />
-  <invoice>
-    <InvoiceNo>12345</InvoiceNo>
-    <InvoiceType>IN</InvoiceType>
-    <InvoiceTypeDesc>Invoice</InvoiceTypeDesc>
-    <InvoiceDate>20170202</InvoiceDate>
-    <InvoiceDueDate>20170302</InvoiceDueDate>
-    <Amount>1000</Amount>
-    <Balance>123.22</Balance>
-  </invoice>
-  <invoice>
-    <InvoiceNo>12346</InvoiceNo>
-    <InvoiceType>PP</InvoiceType>
-    <InvoiceTypeDesc>Prepay</InvoiceTypeDesc>
-    <InvoiceDate>20170205</InvoiceDate>
-    <InvoiceDueDate>20170205</InvoiceDueDate>
-    <Amount>-500</Amount>
-    <Balance>-500</Balance>
-  </invoice>
-  <invoice>
-    <InvoiceNo>12444</InvoiceNo>
-    <InvoiceType>IN</InvoiceType>
-    <InvoiceTypeDesc>Invoice</InvoiceTypeDesc>
-    <InvoiceDate>20170202</InvoiceDate>
-    <InvoiceDueDate>20170302</InvoiceDueDate>
-    <Amount>250</Amount>
-    <Balance>22.00</Balance>
-  </invoice>
-</rosetta>';
-
+        $security_key = $this->config['security_key'];
         $result = array();
+        $customer_id = (UserMeta::instance())->get_sage_id(get_current_user_id());
+        $request_xml = "<rosetta>
+                          <securitykey>{$security_key}</securitykey>
+                          <function>GetOpenInvoiceData</function>
+                          <xml>
+                            <ExtSourceID>YODERWEB</ExtSourceID>
+                            <ARDivisionNo>00</ARDivisionNo>
+                            <CustomerNo>{$customer_id}</CustomerNo>
+                          </xml>
+                        </rosetta>";
+
+        $response = $this->yoder_remote_get($request_xml);
+
+        if (!empty($response['error_message'])) {
+            $result['error_message'] = $response['error_message'];
+            $result['invoices'] = array();
+            return $result;
+        }
+
+        $xml = $response['response'];
         $xml = @simplexml_load_string($xml);
         if ($xml === false) {
             throw new \Exception('Invalid XML response');
         }
-        $xml_array = Helper::xmlToArray($xml);
+        $xml_array = (Helper::instance())->xmlToArray($xml);
 
         if ($xml_array['rosetta']['ErrorStatus'] != 0) {
             $result['error_message'] = $xml_array['rosetta']['ErrorMessage'];
             $result['invoices'] = array();
         } else {
-            $result['error_message'] = array();
+            $result['error_message'] = '';
             $result['invoices'] = $xml_array['rosetta']['invoice'];
         }
 
         return $result;
     }
 
-    public function get_paid_inovices() {
-        
-    }
-
-    public function get_non_paid_invoices()
+    /**
+     * Get due invoices.
+     */
+    public function get_due_invoices()
     {
         $invoices = $this->get_invoices();
 
@@ -160,56 +122,176 @@ $xml = '<rosetta>
                     unset($invoices['invoices'][$key]);
                 }
             }
-
-            return $invoices;
         }
+
+        $invoices = $this->filter_invoices($invoices);
+        return $invoices;
     }
 
+
+    /**
+     * Filter invoices that are already paid.
+     *
+     * @param Array $invoices
+     * @return Array
+     */
+    public function filter_invoices($invoices)
+    {
+        $user_data = (UserMeta::instance())->get_user_invoice_data(get_current_user_id());
+        $user_saved_invoices = $user_data['invoice'];
+
+        if (empty($user_saved_invoices)) {
+            return $invoices;
+        } else {
+            foreach ($invoices['invoices'] as $key => $invoice) {
+                if (in_array($invoice['InvoiceNo'], $user_saved_invoices)) {
+                    unset($invoices['invoices'][$key]);
+                }
+            }
+        }
+
+        return $invoices;
+    }
+
+    /**
+     * Get customer information.
+     */
     public function get_customer()
     {
-        $xml = '<rosetta>
-  <ErrorStatus>0</ErrorStatus>
-  <ErrorMessage />
-  <cust>
-    <ARDivisionNo>00</ARDivisionNo>
-    <CustomerNo>0000003</CustomerNo>
-    <CustomerName>Kalispell 3rd Ave.</CustomerName>
-    <AddressLine1>***credit card clearing***</AddressLine1>
-    <AddressLine2/>
-    <AddressLine3/>
-    <City>Kalispell</City>
-    <State>MT</State>
-    <ZipCode>59901</ZipCode>
-    <CountryCode>USA</CountryCode>
-    <TelephoneNo/>
-    <TelephoneExt/>
-    <FaxNo/>
-    <EmailAddress/>
-    <CurrentBalance>1310.39</CurrentBalance>
-    <PastDueBalance>0</PastDueBalance>
-  </cust>
-</rosetta>';
-
+        $security_key = $this->config['security_key'];
         $result = array();
+        $customer_id = (UserMeta::instance())->get_sage_id(get_current_user_id());
+        $request_xml = "<rosetta>
+                    <securitykey>{$security_key}</securitykey>
+                      <function>GetCustomerData</function>
+                      <xml>
+                        <ExtSourceID>YODERWEB</ExtSourceID>
+                        <ARDivisionNo>00</ARDivisionNo>
+                        <CustomerNo>{$customer_id}</CustomerNo>
+                      </xml>
+                </rosetta>";
+
+        $response = $this->yoder_remote_get($request_xml);
+
+        if (!empty($response['error_message'])) {
+            $result['error_message'] = $response['error_message'];
+            $result['customer'] = array();
+            return $result;
+        }
+
+
+        $xml = $response['response'];
         $xml = @simplexml_load_string($xml);
         if ($xml === false) {
             throw new \Exception('Invalid XML response');
         }
-        $xml_array = Helper::xmlToArray($xml);
+        $xml_array = (Helper::instance())->xmlToArray($xml);
 
         if ($xml_array['rosetta']['ErrorStatus'] != 0) {
             $result['error_message'] = $xml_array['rosetta']['ErrorMessage'];
             $result['customer'] = array();
         } else {
-            $result['error_message'] = array();
+            $result['error_message'] = '';
             $result['customer'] = $xml_array['rosetta']['cust'];
         }
 
         return $result;
     }
+
+    /**
+     * Call Rosetta/PDI API via Socket to get the response.
+     *
+     * @param string $request_message
+     * @param Array
+     */
+    public function yoder_remote_get($request_message)
+    {
+        $host = $this->config['ip'];
+        $port = $this->config['port'];
+        $bytes = 1024;
+        $response_data = '';
+
+        // Usually an XML to send to rosetta server.
+        $message = trim($request_message);
+
+        $response = array(
+            'error_message' => '',
+            'response' => ''
+        );
+
+        // Create socket.
+        $socket = @socket_create(AF_INET, SOCK_STREAM, 0);
+        if ($socket === false) {
+            $error_code = socket_last_error();
+            $error_msg = socket_strerror($error_code);
+            $error_message = "Clouldn't create socket: [{$error_code}] {$error_msg}";
+
+            $this->logger->log($error_message);
+
+            $response['error_message'] = $error_message;
+            $response['response'] = '';
+            return $response;
+        }
+
+        // Connect socket.
+        $socket_connection = @socket_connect($socket, $host, $port);
+        if ($socket_connection === false) {
+            $error_code = socket_last_error();
+            $error_msg = socket_strerror($error_code);
+            $error_message = "Clouldn't create socket connection: [{$error_code}] {$error_msg}";
+
+            $this->logger->log($error_message);
+
+            $response['error_message'] = $error_message;
+            $response['response'] = '';
+            return $response;
+        }
+
+        // Send data to rosetta server via socket.
+        $socket_write = @socket_write($socket, $message, strlen($message));
+        if ($socket_write === false) {
+            $error_code = socket_last_error();
+            $error_msg = socket_strerror($error_code);
+            $error_message = "Clouldn't write to socket connection: [{$error_code}] {$error_msg}";
+
+            $this->logger->log($error_message);
+
+            $response['error_message'] = $error_message;
+            $response['response'] = '';
+            return $response;
+        }
+
+        // Read response from rosetta server via socket.
+        while (($buffer = socket_read($socket, $bytes))) {
+            if ($buffer === false) {
+                $error_code = socket_last_error();
+                $error_msg = socket_strerror($error_code);
+                $error_message = "Error while reading data from socket: [{$error_code}] {$error_msg}";
+
+                $this->logger->log($error_message);
+
+                $response['error_message'] = $error_message;
+                $response['response'] = '';
+                return $response;
+            }
+
+            $response_data .= $buffer;
+        }
+
+        socket_close($socket);
+
+        $response['error_message'] = '';
+        $response['response'] = $response_data;
+        return $response;
+    }
 }
 
-function dd($mix) {
+/**
+ * Debugging    
+ * @param  mix $mix
+ */
+function dd($mix)
+{
     echo "<pre>";
     print_r($mix);
     echo "</pre>";
