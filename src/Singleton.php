ffea76d76aba04a20f221ec2c5f39be19ebbfe87
Awais <awais300@gmail.com>
2022-07-03 09:39:46 +0500

    apply formatting

--- src/Singleton.php
+++ src/Singleton.php
@@ -2,7 +2,7 @@
 
 namespace Yoder\YIPS;
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 /**
  * Base class for singleton objects.
@@ -10,36 +10,46 @@ defined( 'ABSPATH' ) || exit;
  * @package Yoder\YIPS
  * @abstract
  */
-abstract class Singleton {
+abstract class Singleton
+{
 
 	private static $instances = array();
-	protected function __construct() {}
-	protected function __clone() {}
-	public function __wakeup() {
-		throw new \Exception( 'Cannot unserialize singleton' );
+	protected function __construct()
+	{
+	}
+	protected function __clone()
+	{
+	}
+	public function __wakeup()
+	{
+		throw new \Exception('Cannot unserialize singleton');
 	}
 
 
 	/**
 	 * Get instance object.
 	 **/
-	public static function get_instance() {
+	public static function get_instance()
+	{
 		$cls = get_called_class(); // late-static-bound class name
-		if ( ! isset( self::$instances[ $cls ] ) ) {
-			self::$instances[ $cls ] = new static;
-			( self::$instances[ $cls ] )->on_construct();
+		if (!isset(self::$instances[$cls])) {
+			self::$instances[$cls] = new static;
+			(self::$instances[$cls])->on_construct();
 		}
-		return self::$instances[ $cls ];
+		return self::$instances[$cls];
 	}
 
-	function on_construct() {}
+	function on_construct()
+	{
+	}
 
 	/**
 	 * Returns the singleton instance.
 	 *
 	 * @return $this
 	 */
-	public static function instance() {
-		 return static::get_instance();
+	public static function instance()
+	{
+		return static::get_instance();
 	}
 }
