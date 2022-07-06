454f5b058c2e66cf68ba69a8a2f8166b96508789
Awais <awais300@gmail.com>
2022-07-07 01:58:00 +0500

    add links

--- templates/thank-you.php
+++ templates/thank-you.php
@@ -1,1 +1,9 @@
-<h1><?php _e('Thank you for your payment.', 'yips-customization'); ?></h1>
+<div class="yoder-thank-you">
+	<h1><?php _e('Thank you for your payment.', 'yips-customization'); ?></h1>
+
+	<div class="yoder-user-links">
+		<a href="<?php echo esc_url($main_screen_url); ?>" title="<?php _e('Main Screen', 'yips-customization'); ?>">Main Screen</a>
+		<br />
+		<a href="<?php echo esc_url($logout_url); ?>" title="<?php _e('Logout'); ?>">Logout</a>
+	</div>
+</div>
