8d9bba856b3e218741c4091f5d063c63be4c322d
Muhammad Awais <mawais@updraftplus.com>
2022-12-10 15:53:25 +0500

    minor text update

--- templates/thank-you.php
+++ templates/thank-you.php
@@ -2,6 +2,7 @@
 	<h1><?php _e('We have received your payment.', 'yips-customization'); ?></h1>
 
 	<div class="yoder-user-links">
+	<p>A receipt has been emailed.</p>
 		<p><?php _e('Would you like to return to the invoice screen', 'yips-customization'); ?> <span><a href="<?php echo esc_url($main_screen_url); ?>" title="<?php _e('Invoice Screen', 'yips-customization'); ?>">Invoice Screen</a></span> or
 			<span><a href="<?php echo esc_url($logout_url); ?>" title="<?php _e('Logout', 'yips-customization'); ?>">Logout</a>?</span>
 		</p>
